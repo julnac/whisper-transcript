e88a261cb310a4505e33d15ad16c5384e241dc77
Grzegorz Pozorski <grzegorz.pozorski@gmail.com>
2023-08-06 16:54:52 +0200

    ready home page with send btn

--- my-app/src/components/drop-file-input/DropFileInput.jsx
+++ my-app/src/components/drop-file-input/DropFileInput.jsx
@@ -1,9 +1,10 @@
-import React, { useRef, useState } from 'react';
+import React, {useRef, useState} from 'react';
 import PropTypes from 'prop-types'
 
 import "./drop-file-input.css";
+import "../../styles/App.css";
 
-import Mp3 from '../../assets/mp3.png'; 
+import Mp3 from '../../assets/mp3.png';
 import UploadImg from '../../assets/upload.svg';
 
 
@@ -12,74 +13,97 @@ const DropFileInput = () => {
     const wrapperRef = useRef(null);
 
     const [file, setFile] = useState(null);
-    const [disableFileInput, setDisableFileInput] = useState(false);
+    const [hideFileInput, setHideFileInput] = useState(false);
+    const [isDisabled, setDisabled] = useState(true);
+
     const onDragEnter = () => wrapperRef.current.classList.add('dragover');
 
     const onDragLeave = () => wrapperRef.current.classList.remove('dragover');
 
     const onDrop = () => wrapperRef.current.classList.remove('dragover');
 
+    const onTokenInput = (event) => {
+        const input = event.target.value;
+        if (input.length ===6) {
+            setDisabled(false);
+        } else {
+            setDisabled(true);
+        }
+    };
+
     const onFileDrop = (e) => {
         const allowedExtensions = /(\.mp3)$/i;
-        // const fileInput = document.getElementById('file');
-        // const filePath = fileInput.value;
         const newFile = e.target.files[0];
-
-        wrapperRef.current.classList.add('dragover');
 
         if (!allowedExtensions.exec(newFile.name)) {
             alert('Invalid file type');
             return false;
         }
 
-        if(newFile !== undefined && newFile !== null){
-            // validate
-            setDisableFileInput(true);
-            setFile(newFile);
-            return true;
-        }
+        setHideFileInput(true);
+        setFile(newFile);
+        return true;
     }
 
     const fileRemove = () => {
         setFile(null);
-        setDisableFileInput(false);
-        wrapperRef.current.classList.remove('dragover');
-        
+        setHideFileInput(false);
+        setHideFileInput(false);
+        setFile(null);
+    }
+
+    const getFileSize = (_size) => {
+        let i = 0;
+        const fSExt = ['Bytes', 'KB', 'MB', 'GB'];
+
+        while (_size > 900) {
+            _size /= 1024;
+            i++;
+        }
+        return (Math.round(_size * 100) / 100) + ' ' + fSExt[i];
     }
 
     return (
         <>
-            <div
-                ref={wrapperRef}
-                className="drop-file-input"
-                onDragEnter={onDragEnter}
-                onDragLeave={onDragLeave}
-                onDrop={onDrop}
-                onFileDrop={onFileDrop}
-            >
-                <div className="drop-file-input__label">
-                    <button className='button'>
-                        <img src={UploadImg} alt="Upload Icon" className="download_icon"/>
-                        <p>Wgraj Plik</p>
-                    </button>
-                    <p className='podpis'>kliknij aby przeglądać, lub <br />przeciągnij i upuść tutaj</p>
-                </div>
-                <input type="file" value="" onChange={onFileDrop}  disabled={disableFileInput}/>
-            </div>
+            {
+                !hideFileInput
+                    ?
+                    <div
+                        ref={wrapperRef}
+                        className="drop-file-input"
+                        onDragEnter={onDragEnter}
+                        onDragLeave={onDragLeave}
+                        onDrop={onDrop}
+                        onFileDrop={onFileDrop}
+                    >
+                        <div className="drop-file-input__label">
+                            <button className='button button__download'>
+                                <img src={UploadImg} alt="Upload Icon" className="download_icon"/>
+                                <p>Wgraj Plik</p>
+                            </button>
+                            <p className='podpis'>kliknij aby przeglądać, lub <br />przeciągnij i upuść tutaj</p>
+                        </div>
+                        <input type="file" value="" onChange={onFileDrop}  disabled={hideFileInput}/>
+                    </div>
+                    :
+                    <></>
+            }
             {
                 file !== null ? (
                     <div className="drop-file-preview">
-                        <p className="drop-file-preview__title">
-                            Gotowe do przesłania
-                        </p>
                             <div className="drop-file-preview__item ">
-                                <img src={Mp3} className='img_mp3'/>
+                                <img src={Mp3} className='img_mp3' alt={"mp3"}/>
                                 <div className="drop-file-preview__item__info">
                                     <p>{file.name}</p>
-                                    <p>{file.size}B</p>
+                                    <p>{getFileSize(file.size)}</p>
                                 </div>
                                 <span className="drop-file-preview__item__del" onClick={() => fileRemove()}>x</span>
                             </div>
+                        <p className="drop-file-preview__title">Wprowadź token</p>
+                        <form className="container">
+                            <input type="text" onChange={onTokenInput} className="input__token" placeholder="Bearer token"/><br />
+                            <button className="button button__send" disabled={isDisabled}>Generuj</button>
+                        </form>
                     </div>
                 ) : null
             }
